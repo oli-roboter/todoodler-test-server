28f460b95a5b389af915d13c9a99a14503ad6312
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-09-03 18:00:09 +0100

    initial setup

---
+++ README.md
@@ -0,0 +1,2 @@
+# todoodler-test-server
+Server to test if proxy pass of architecture in EC2 is working
---
+++ app.js
@@ -0,0 +1,11 @@
+import express from 'express';
+const app = express();
+
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
+// app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, '../public')));
+
+app.get(`/test`, (req, res) => res.send('Test successfull'));
+
+const server = app.listen(3000, () => console.log('App listening on port 3000'));
