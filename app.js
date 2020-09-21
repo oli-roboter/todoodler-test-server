560d6e80ebfc588c4698ffff974e8fad7a499316
Oliver Wagner <oliverthomas.wagner@gmail.com>
2020-09-21 16:02:19 +0100

    added dotenv package

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 import express from 'express';
 const app = express();
 
+require('dotenv').config();
+
+const TEST = process.env.TEST_ENV_VARIABLE || 'DEFAULT'
+
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 // app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, '../public')));
 
-app.get(`/test`, (req, res) => res.send('Test successfull, environment variable is:', process.env.TEST_ENV_VARIABLE));
+app.get(`/test`, (req, res) => res.send(`Test successfull, environment variable is:, ${TEST}`));
 
 const server = app.listen(3000, () => console.log('App listening on port 3000'));
